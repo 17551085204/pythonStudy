a6aee0658ed64d6a38b2983571bfe52da0c2bc73
hdk <2890241339@qq.com>
2021-03-01 21:09:21 +0800

    1
    1

--- python36/数据结构与算法/韩顺平/07查找/SearchDemo.py
+++ python36/数据结构与算法/韩顺平/07查找/SearchDemo.py
@@ -68,7 +68,6 @@ def binarySearch2(array,left,right,x):
     # 插值查找
     mid=left+(right-left)*(x-array[left])//(array[right]-array[left])
 
-
     res=[]
     if array[mid]==x:
         temp=mid-1
@@ -93,13 +92,62 @@ def binarySearch2(array,left,right,x):
     elif array[mid]>x:
         return binarySearch2(array, left, mid-1, x)
 
+# 斐波那契查找算法
+# 返回斐波那契数组的第k个元素
+def fib(k):
+    if k==1 or k==2:
+        return 1
+    return fib(k-1)+fib(k-2)
+# 斐波那契数组查找算法
+def fibSearch(array,x):
+    left = 0
+    right = len(array) - 1
+    k=1 # 斐波那契分割数值对应的序号
+    res=[]
+    while right>fib(k)-1:
+        k=k+1
+    temp=[]
+    for i in array:
+        temp.append(i)
+    for i in range(fib(k)-len(array)):
+        temp.append(array[-1])
+
+    while left<=right:
+        mid=left+fib(k-1)-1
+        if x<temp[mid]:
+            right=mid-1
+            k=k-1
+        elif x>temp[mid]:
+            left=mid+1
+            k=k-2
+        else:
+            if right<=mid:
+                mid=right
+
+            te = mid - 1
+            while array[mid] == x:
+                res.append(mid)
+                mid = mid + 1
+                if mid >= len(array):
+                    break
+            while array[te] == x:
+                res.append(te)
+                te = te - 1
+                if te < 0:
+                    break
+            if len(res) == 1:
+                return res[0]
+            else:
+                return res
+
 
 if __name__ == '__main__':
-    array=[0,1,2,3,4,5,6,7,8,8,19]
+    array=[0,1,2,3,4,5,6,7,8,8,8]
     val=8
     print(seqSearch(array,val))
     print(binarySearch(array, val))
     print(binarySearch2(array,0,len(array)-1,val))
+    print(fibSearch(array,val))
 
 
 
